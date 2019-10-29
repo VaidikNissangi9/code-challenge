87f85b5e8dbdd0feb0c54475a46e6c3e6a07383d
VaidikNissangi9 <vaidik.nissangi@zemosolabs.com>
2019-10-29 15:37:49 +0530

    few changes

--- maxPath.py
+++ maxPath.py
@@ -1,28 +1,14 @@
-string = '''75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'''
+string = '[3],[7,4],[2,4,6],[8,5,9,3] '
 strs = string.replace('[','').split('],')
 lists = [map(int, s.replace(']','').split(',')) for s in strs]
 
 def maxPath(lists, index=-1):
     if index == - len(lists):
         return lists[0][0]
-    for index in range(len(lists[start_index]) - 1):
-        maximum = max(lists[start_index][index], lists[start_index][index + 1])
-        lists[start_index - 1][index] += maximum
-    return maxPath(lists, start_index - 1)
+    for i in range(len(lists[index]) - 1):
+        maximum = max(lists[index][i], lists[index][i+1])
+        lists[index- 1][i] += maximum
+    return maxPath(lists, index-1)
 
 print(maxPath(lists))
 
