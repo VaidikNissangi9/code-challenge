f792fd3384f552fdd76264b776b8ed3f0a313060
VaidikNissangi9 <vaidik.nissangi@zemosolabs.com>
2019-10-29 17:23:09 +0530

    first commit

--- maxPath.py
+++ maxPath.py
@@ -1,9 +1,12 @@
-string = '[3],[7,4],[2,4,6],[8,5,9,3] '
-strings = string.replace('[','').split('],')
-lists = [map(int, s.replace(']','').split(',')) for s in strings]
-
-def maxPath(lists, index=-1):
-    
+string = '''3
+7 4
+2 4 6
+8 5 9 3'''
+strings = string.split('\n')
+lists=[map(int,w.split(' ')) for w in strings ]
+def maxPath(lists, index=-1):  
+    if index==-len(lists) :
+        return lists[0][0]
     for i in range(len(lists[index]) - 1):
         maxSum = max(lists[index][i], lists[index][i+1])
         lists[index- 1][i] += maxSum
